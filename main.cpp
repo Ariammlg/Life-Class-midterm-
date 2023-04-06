c734c6693955030eaa90cc59d79f2227767aa795
Aria Bagheri <Aryammlg@gmail.com>
2023-04-06 20:51:14 +0430

    initial steps

--- main.cpp
+++ main.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
+#include <vector>
+#include <cmath>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
-}
+using std:: cout, std::cin, std::endl, std::vector, std::string;
+
+
+class Genome{
+public:
+    vector<char> RNA;
+    vector<char> DNA;
+
+    void receive(string s){
+        if(s.find('\n') == string::npos){
+            for(int i=0; i<s.size();i++){
+                RNA.push_back(s[i]);
+            }
+        }
+        else{
+            for(int i=0; i < s.size();i++){
+                DNA.push_back(s[i]);
+            }
+        }
+    }
+
+
+
+
+};
+
+
